0acba4a81697eedd7bab80ec71f736bbc1715f48
kfwong <kfwong@zoho.com>
2016-09-07 07:55:58 +0800

    Added test case for exact match.

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -8,15 +8,25 @@ import static org.junit.Assert.*;
 
 public class NameTest {
     private Name name;
+    
+    private Name name1; // John Smith
 
     @Before
     public void setup() throws IllegalValueException{
         name = new Name("John Smith");
+        name1 = new Name("John Smith");
     }
     
     @Test(expected=NullPointerException.class)
     public void isSimilar_nameIsNull(){
         boolean result = name.isSimilar(null);
         
+    }
+    
+    @Test
+    public void isSimilar_exactMatch(){
+        boolean result = name.isSimilar(name1);
+        
+        assertTrue("isSimilar: Exact match", result);
     }
 }
