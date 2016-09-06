61e568b6cce9de480b9f1ba3acf399455fe6c115
kfwong <kfwong@zoho.com>
2016-09-06 20:46:13 +0800

    Extract commonalities from Phone, Email and Address classes into Contact

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,16 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+
+    public final String value;
+    private boolean isPrivate;
+    
+    public Contact(String value, boolean isPrivate){
+        this.value = value;
+        this.isPrivate = isPrivate;
+    }
+    
+    public boolean isPrivate(){
+        return isPrivate;
+    }
+}
